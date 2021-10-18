562ebc02ac85b3d6bdf673375e2fed38a412e952
vadimsavenkov <vadim.y.savenkov@gmail.com>
2021-10-18 12:14:18 -0600

    Update script.js
    Declare mutable/non-mutable variables. Difference let vs const vs var

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -4,7 +4,7 @@ console.log(40 + 8 + 23 - 10);
 
 let firstName = 'Vlad';
 console.log(firstName);
-*/
+
 let javascriptIsFun = true;
 console.log(javascriptIsFun)
 
@@ -20,3 +20,23 @@ console.log(typeof year);
 
 year = 1991;
 console.log(typeof year);
+*/
+
+//mutate variable
+let age = 30;
+age = 31;
+
+// immutable variable
+const birthYear = 1991;
+birthYear = 1990;
+
+// var is an old way to define variable
+var job = 'programmer';
+job = 'teacher';
+
+//never use var!
+
+lastName = 'Savenkov';
+console.log(lastName);
+
+//you should always declare variable!
