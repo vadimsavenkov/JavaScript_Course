df8d2ef50e5621a6eb3e87d24005d102b73bf947
vadimsavenkov <vadim.y.savenkov@gmail.com>
2021-10-20 21:07:20 -0600

    Update assignment.js
    add coding challenges as a part of assignment

--- 01-Fundamentals-Part-1/starter/assignment.js
+++ 01-Fundamentals-Part-1/starter/assignment.js
@@ -1,6 +1,38 @@
+/*
 let country = ('Canada');
 let continent = ('North America');
 let population = ('40,000,000.0')
 console.log(country);
 console.log(continent);
 console.log(population);
+
+// first code challenge
+const massMark = 78;
+const heightMark = 1.69;
+const massJohn = 92;
+const heightJohn = 1.95;
+
+const bmiMark = massMark / heightMark ** 2;
+const bmiJohn = massJohn / heightJohn ** 2;
+const markHigherBMI = bmiMark > bmiJohn;
+
+console.log(bmiMark, bmiJohn, markHigherBMI);
+
+
+/ second code challenge
+const massMark = 78;
+const heightMark = 1.69;
+const massJohn = 120;
+const heightJohn = 1.95;
+const bmiMark = massMark / heightMark ** 2;
+const bmiJohn = massJohn / heightJohn ** 2;
+
+console.log(bmiMark, bmiJohn);
+
+if (bmiMark > bmiJohn) {
+    console.log(`Mark's BMI(${bmiMark}) is higher than John's(${bmiJohn})!`)
+} else {
+    console.log(`Mark's BMI(${bmiMark}) is lower than John's(${bmiJohn})!`)
+}
+
+*/
