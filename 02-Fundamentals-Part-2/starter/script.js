ad4c4616c52e4a021dc81016ebb0cc0b57701a83
vadimsavenkov <vadim.y.savenkov@gmail.com>
2021-10-22 19:02:58 -0600

    Update script.js
    function declaration vs function expression

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -12,7 +12,7 @@ if (hasDriversLicense) console.log('I can drive!');
 
 //const interface = 'Audio';
 //const private = 534;
-*/
+
 function logger() {
     console.log('My name is Vadim!')
 }
@@ -40,4 +40,18 @@ console.log(appleOrangeJuice);
 const num = Number('25');
 console.log(num);
 
+//another example of function declaration. You can call it before defining
+function calcAge1(birthYear) {
+    return 2037 - birthYear;
+}
+const age1 = calcAge1(1991);
+console.log(age1)
+//function expression
+const calcAge2 = function (birthYear) {
+    return 2037 - birthYear;
+}
+const age2 = calcAge2(1991);
+console.log(age1, age2)
+
+*/
 
