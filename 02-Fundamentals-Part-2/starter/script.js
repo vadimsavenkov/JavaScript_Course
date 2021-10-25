e49325305598cf3dab415bf87007c47425ce916e
vadimsavenkov <vadim.y.savenkov@gmail.com>
2021-10-24 20:57:10 -0600

    Update script.js
    using for loop

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -386,3 +386,20 @@ if (dataMark.bmi > dataJohn.bmi) {
 };
 
 */
+
+//loops
+// console.log('Lifting weights repetition 1 ');
+// console.log('Lifting weights repetition 2 ');
+// console.log('Lifting weights repetition 3 ');
+// console.log('Lifting weights repetition 4 ');
+// console.log('Lifting weights repetition 5 ');
+// console.log('Lifting weights repetition 6 ');
+// console.log('Lifting weights repetition 7 ');
+// console.log('Lifting weights repetition 8 ');
+// console.log('Lifting weights repetition 9 ');
+// console.log('Lifting weights repetition 10 ');
+
+//for loop keeps running while condition TRUE!
+for (let rep = 1; rep <= 10; rep++) {
+    console.log(`Lifting weights repetition ${rep}`);
+}
