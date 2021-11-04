f8a96aa13248389c0e4cee2d92a97ce926a2b3b0
vadimsavenkov <vadim.y.savenkov@gmail.com>
2021-11-03 19:33:21 -0600

    Create script.js
    First commit

---
+++ 16-Asynchronous/starter/script.js
@@ -0,0 +1,9 @@
+/*
+'use strict';
+
+const btn = document.querySelector('.btn-country');
+const countriesContainer = document.querySelector('.countries');
+
+
+///////////////////////////////////////
+*/
