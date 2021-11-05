f8d6d83d954983c9297b4e63d15a32e2c6b682ba
vadimsavenkov <vadim.y.savenkov@gmail.com>
2021-11-04 21:58:27 -0600

    Update script.js
    first commit - XMLHttpRequest

--- 16-Asynchronous/starter/script.js
+++ 16-Asynchronous/starter/script.js
@@ -7,3 +7,13 @@ const countriesContainer = document.querySelector('.countries');
 
 ///////////////////////////////////////
 */
+
+//old way hadling asyns js
+const request = new XMLHttpRequest()
+request.open('GET', 'https://restcountries.com/v2/name/portugal')
+request.send()
+console.log(request.responseText);
+
+request.removeEventListener('load', function() {
+    console.log(this.responseText);
+})
